b087965000534cc352b5305254f60245f88524b8
AC <ac@bidon.org>
2022-07-28 10:28:06 +0200

    publish improvements

--- buildSrc/src/main/kotlin/publish-adapter.gradle.kts
+++ buildSrc/src/main/kotlin/publish-adapter.gradle.kts
@@ -22,27 +22,13 @@ afterEvaluate {
 
         publications {
             register<MavenPublication>("gpr") {
-//                afterEvaluate {
-//                    from(components["release"])
-//                }
+                afterEvaluate {
+                    from(components["release"])
+                }
                 groupId = "com.appodealstack.bidon" // Replace with group ID
                 artifactId = getArtifactId
                 version = getVersionName
                 artifact("$buildDir/outputs/aar/${project.name}-release.aar")
-
-                pom.withXml {
-                    val dependenciesNode = asNode().appendNode("dependencies")
-                    configurations.getByName("implementation") {
-                        dependencies.forEach {
-                            val dependencyNode = dependenciesNode.appendNode("dependency")
-                            dependencyNode.appendNode("groupId", it.group)
-                            dependencyNode.appendNode("artifactId", it.name)
-                            dependencyNode.appendNode("version", it.version)
-                        }
-                    }
-                }
-
-                components["android"]
             }
         }
     }
