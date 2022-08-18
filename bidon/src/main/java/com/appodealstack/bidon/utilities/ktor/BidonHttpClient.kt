3afa9fbc4f92212612d750ca25dcb84f85d8a2bf
AC <ac@bidon.org>
2022-08-18 17:53:49 +0200

    Retry-After logic for BidonHttpClient

--- bidon/src/main/java/com/appodealstack/bidon/utilities/ktor/BidonHttpClient.kt
+++ bidon/src/main/java/com/appodealstack/bidon/utilities/ktor/BidonHttpClient.kt
@@ -1,6 +1,7 @@
 package com.appodealstack.bidon.utilities.ktor
 
 import com.appodealstack.bidon.BuildConfig
+import com.appodealstack.bidon.core.ext.logInfo
 import io.ktor.client.*
 import io.ktor.client.engine.okhttp.*
 import io.ktor.client.plugins.*
@@ -8,6 +9,7 @@ import io.ktor.client.plugins.compression.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.plugins.logging.*
 import io.ktor.client.request.*
+import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
@@ -16,7 +18,7 @@ import kotlinx.serialization.json.Json
 val BidonHttpClient by lazy {
     HttpClient(OkHttp) {
         install(ContentEncoding) {
-             gzip()
+            gzip()
         }
         install(Logging) {
             logger = Logger.ANDROID
@@ -32,11 +34,26 @@ val BidonHttpClient by lazy {
                 }
             )
         }
+        install(HttpRequestRetry) {
+            var retryDelay: Long? = null
+            retryIf { _, response ->
+                (!response.status.isSuccess() && response.headers.contains(HttpHeaders.RetryAfter)).also {
+                    retryDelay = response.headers[HttpHeaders.RetryAfter]?.toLongOrNull()?.let { headerRetryDelay ->
+                        // if [Retry-After] is in seconds, it < 100, else it has to be milliseconds.
+                        headerRetryDelay.takeIf { it > 100L } ?: headerRetryDelay.times(1000)
+                    }
+                    logInfo(Tag, "Request failed. Retry after $retryDelay ms.")
+                }
+            }
+            delayMillis(respectRetryAfterHeader = false) { retry ->
+                retryDelay ?: retry.toLong()
+            }
+        }
         defaultRequest {
-            // TODO add support Retry-After header field (milliseconds).
             header("X-BidOn-Version", BidOnSdkVersion)
         }
     }
 }
 
 private val BidOnSdkVersion by lazy { BuildConfig.ADAPTER_VERSION }
+private const val Tag = "BidonHttpClient"
