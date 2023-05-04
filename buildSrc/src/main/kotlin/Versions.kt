7d22ee9dcf454d96490189d22fbce586d1a83eb5
AC <ac@bidon.org>
2023-05-04 10:25:40 +0200

    Added banner in XML

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -2,7 +2,7 @@ object Versions {
     private val major = 0
     private val minor = 2
     private val patch = 1
-    private val semantic: String? = "-beta.4"
+    private val semantic: String? = "-beta.5"
 
     val BidonVersionName = mainVersion + semanticVersion
 
