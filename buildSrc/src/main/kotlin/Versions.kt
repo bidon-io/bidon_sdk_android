45c200ff4f76aa2e67fa49f9861295ef273f76aa
AC <ac@bidon.org>
2023-05-26 16:25:49 +0200

    BDN-212. Added Javadoc/Dokka (#79)

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -2,7 +2,7 @@ object Versions {
     private val major = 0
     private val minor = 2
     private val patch = 1
-    private val semantic: String? = "-beta.15"
+    private val semantic: String? = "-beta.16"
 
     val BidonVersionName = mainVersion + semanticVersion
 
--- buildSrc/src/main/kotlin/publish-adapter.gradle.kts
+++ buildSrc/src/main/kotlin/publish-adapter.gradle.kts
@@ -1,13 +1,32 @@
+import java.io.FileInputStream
+import java.util.Properties
+
 plugins {
     id("maven-publish")
+    id("common") apply false
 }
-val githubProperties = java.util.Properties()
+
+val githubProperties = Properties()
 val githubCredentialFile: File = rootProject.file("github.properties")
 if (githubCredentialFile.exists()) {
-    githubProperties.load(java.io.FileInputStream(githubCredentialFile))
+    githubProperties.load(FileInputStream(githubCredentialFile))
 }
 
 afterEvaluate {
+    val dokkaJar by tasks.registering(Jar::class) {
+        group = "documentation"
+        dependsOn(tasks.getByName("dokkaJavadoc"))
+        include(javadoc.ClassesList.javaDocsAllowList)
+        archiveClassifier.set("javadoc")
+        from("$buildDir/dokka/javadoc")
+    }
+
+    val sourcesJar by tasks.registering(Jar::class) {
+        group = "documentation"
+        archiveClassifier.set("sources")
+        include(javadoc.ClassesList.javaDocsAllowList)
+        from(android.sourceSets.getByName("main").java.srcDirs)
+    }
     configure<PublishingExtension> {
         val getArtifactId = project.getArtifactId()
         val getVersionName = project.getVersionName()
@@ -28,6 +47,8 @@ afterEvaluate {
                 afterEvaluate {
                     from(components["productionRelease"])
                 }
+                artifact(dokkaJar)
+                artifact(sourcesJar)
                 groupId = "org.bidon" // Replace with group ID
                 artifactId = getArtifactId
                 version = getVersionName
