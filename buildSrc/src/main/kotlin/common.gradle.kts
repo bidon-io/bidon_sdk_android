75949620fc367a22b6a92a8a0b627f471048bea5
AC <ac@bidon.org>
2023-08-03 14:01:26 +0200

    BDN-330. Kotlin BOM. (#115)
    * BDN-330. Kotlin BOM.
    
    * BDN-330. Release 0.3.0

--- buildSrc/src/main/kotlin/common.gradle.kts
+++ buildSrc/src/main/kotlin/common.gradle.kts
@@ -44,9 +44,13 @@ android {
 }
 
 dependencies {
+    implementation(platform(Dependencies.Kotlin.bom))
+    implementation(Dependencies.Kotlin.reflect)
+
+    implementation(platform(Dependencies.Kotlin.Coroutines.bom))
     implementation(Dependencies.Kotlin.Coroutines.KotlinxCoroutinesCore)
     implementation(Dependencies.Kotlin.Coroutines.KotlinxCoroutinesAndroid)
-    implementation(Dependencies.Kotlin.reflect)
+
     implementation(Dependencies.Android.CoreKtx)
     implementation(Dependencies.Android.Annotation)
 
@@ -54,12 +58,14 @@ dependencies {
      * Testing
      */
     testImplementation(kotlin("test"))
+    testImplementation(kotlin("test-junit"))
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test")
+
     testImplementation("io.mockk:mockk:1.13.5") {
         exclude(group = "org.slf4j", module = "slf4j-api")
     }
     testImplementation("com.google.truth:truth:1.1.4")
     testImplementation("junit:junit:4.13.2")
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
     testImplementation("org.json:json:20210307")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
