4f79778bf687e7947749f5844cd89e9b38d2431a
AC <ac@bidon.org>
2023-09-10 22:24:48 +0200

    Yeti Ads

---
+++ app/src/main/java/org/bidon/demoapp/LineItems.kt
@@ -0,0 +1,114 @@
+package org.bidon.demoapp
+
+import org.bidon.sdk.auction.models.LineItem
+
+/**
+ * Created by Aleksei Cherniaev on 09/09/2023.
+ *
+ * [Applovin](https://dash.applovin.com/o/zones?r=2)
+ * [UnityAds](https://dashboard.unity3d.com/gaming/organizations/7971940023451/projects/020dc98e-105d-4862-99fd-a452220532cd/monetization/placement-management?adUnits=google_unityads_inter_1_0)
+ * [Vungle-Liftoff](https://publisher.vungle.com/placements)
+ * [BigoAds](https://www.bigossp.com/media/app?pageNo=1&pageSize=10)
+ */
+object LineItems {
+    object Interstitial {
+        val all get() = unityAds + admob + applovin
+
+        private val unityAds = listOf(
+            LineItem(demandId = "unityads", pricefloor = 1.0, adUnitId = "unity_inter_1_"),
+            LineItem(demandId = "unityads", pricefloor = 2.0, adUnitId = "unity_inter_2"),
+            LineItem(demandId = "unityads", pricefloor = 5.0, adUnitId = "unity_inter_5"),
+            LineItem(demandId = "unityads", pricefloor = 7.0, adUnitId = "unity_inter_7"),
+            LineItem(demandId = "unityads", pricefloor = 10.0, adUnitId = "unity_inter_10"),
+            LineItem(demandId = "unityads", pricefloor = 15.0, adUnitId = "unity_inter_15"),
+            LineItem(demandId = "unityads", pricefloor = 20.0, adUnitId = "unity_inter_20"),
+            LineItem(demandId = "unityads", pricefloor = 25.0, adUnitId = "unity_inter_25"),
+            LineItem(demandId = "unityads", pricefloor = 35.0, adUnitId = "unity_inter_35"),
+            LineItem(demandId = "unityads", pricefloor = 50.0, adUnitId = "unity_inter_50"),
+            LineItem(demandId = "unityads", pricefloor = 70.0, adUnitId = "unity_inter_70"),
+            LineItem(demandId = "unityads", pricefloor = 95.0, adUnitId = "unity_inter_95")
+        )
+
+        private val admob = listOf(
+            LineItem(demandId = "admob", pricefloor = 1.0, adUnitId = "ca-app-pub-9069030271740050/2719222246"),
+            LineItem(demandId = "admob", pricefloor = 2.0, adUnitId = "ca-app-pub-9069030271740050/9682455534"),
+            LineItem(demandId = "admob", pricefloor = 5.0, adUnitId = "ca-app-pub-9069030271740050/2527650551"),
+            LineItem(demandId = "admob", pricefloor = 7.0, adUnitId = "ca-app-pub-9069030271740050/4250165457"),
+            LineItem(demandId = "admob", pricefloor = 10.0, adUnitId = "ca-app-pub-9069030271740050/7371701555"),
+            LineItem(demandId = "admob", pricefloor = 15.0, adUnitId = "ca-app-pub-9069030271740050/4553966523"),
+            LineItem(demandId = "admob", pricefloor = 20.0, adUnitId = "ca-app-pub-9069030271740050/4579098822"),
+            LineItem(demandId = "admob", pricefloor = 25.0, adUnitId = "ca-app-pub-9069030271740050/6796986482"),
+            LineItem(demandId = "admob", pricefloor = 35.0, adUnitId = "ca-app-pub-9069030271740050/8326772146"),
+            LineItem(demandId = "admob", pricefloor = 50.0, adUnitId = "ca-app-pub-9069030271740050/5292333129"),
+            LineItem(demandId = "admob", pricefloor = 70.0, adUnitId = "ca-app-pub-9069030271740050/5009509512"),
+            LineItem(demandId = "admob", pricefloor = 95.0, adUnitId = "ca-app-pub-9069030271740050/2091454718")
+        )
+
+        private val applovin = listOf(
+            LineItem(demandId = "applovin", pricefloor = 1.0, adUnitId = "9dd72d785cbc17fe"),
+            LineItem(demandId = "applovin", pricefloor = 2.0, adUnitId = "82f494dc84767bcd"),
+            LineItem(demandId = "applovin", pricefloor = 5.0, adUnitId = "71095830df3679ad"),
+            LineItem(demandId = "applovin", pricefloor = 7.0, adUnitId = "ad6f7fcab26463d5"),
+            LineItem(demandId = "applovin", pricefloor = 10.0, adUnitId = "abdc2d3be8365b37"),
+            LineItem(demandId = "applovin", pricefloor = 15.0, adUnitId = "5d2bb9df44c033b7"),
+            LineItem(demandId = "applovin", pricefloor = 20.0, adUnitId = "5dc9a141c5241bf5"),
+            LineItem(demandId = "applovin", pricefloor = 25.0, adUnitId = "230931c9ca69cfe1"),
+            LineItem(demandId = "applovin", pricefloor = 35.0, adUnitId = "ca86b0231fe94b4d"),
+            LineItem(demandId = "applovin", pricefloor = 50.0, adUnitId = "cd2281f8cfe4d5e7"),
+            LineItem(demandId = "applovin", pricefloor = 70.0, adUnitId = "6ebb8e46b8984c08"),
+            LineItem(demandId = "applovin", pricefloor = 95.0, adUnitId = "ec1e3701da66ef14")
+        )
+    }
+
+    object Banners {
+        val all get() = unityAds + admob + applovin
+
+        private val unityAds = listOf(
+            LineItem(demandId = "unityads", pricefloor = 0.01, adUnitId = "unity_banner_0_01"),
+            LineItem(demandId = "unityads", pricefloor = 0.05, adUnitId = "unity_banner_0_05"),
+            LineItem(demandId = "unityads", pricefloor = 0.1, adUnitId = "unity_banner_0_1"),
+            LineItem(demandId = "unityads", pricefloor = 0.2, adUnitId = "unity_banner_0_2"),
+            LineItem(demandId = "unityads", pricefloor = 0.3, adUnitId = "unity_banner_0_3"),
+            LineItem(demandId = "unityads", pricefloor = 0.5, adUnitId = "unity_banner_0_5"),
+            LineItem(demandId = "unityads", pricefloor = 0.7, adUnitId = "unity_banner_0_7"),
+            LineItem(demandId = "unityads", pricefloor = 1.0, adUnitId = "unity_banner_1_0"),
+            LineItem(demandId = "unityads", pricefloor = 1.5, adUnitId = "unity_banner_1_5"),
+            LineItem(demandId = "unityads", pricefloor = 2.0, adUnitId = "unity_banner_2"),
+            LineItem(demandId = "unityads", pricefloor = 3.0, adUnitId = "unity_banner_3"),
+            LineItem(demandId = "unityads", pricefloor = 4.0, adUnitId = "unity_banner_4")
+        )
+        private val admob = listOf(
+            LineItem(demandId = "admob", pricefloor = 0.01, adUnitId = "ca-app-pub-9069030271740050/3238096991"),
+            LineItem(demandId = "admob", pricefloor = 0.05, adUnitId = "ca-app-pub-9069030271740050/1636444558"),
+            LineItem(demandId = "admob", pricefloor = 0.1, adUnitId = "ca-app-pub-9069030271740050/1444872866"),
+            LineItem(demandId = "admob", pricefloor = 0.2, adUnitId = "ca-app-pub-9069030271740050/7874549840"),
+            LineItem(demandId = "admob", pricefloor = 0.3, adUnitId = "ca-app-pub-9069030271740050/3743733147"),
+            LineItem(demandId = "admob", pricefloor = 0.5, adUnitId = "ca-app-pub-9069030271740050/4426259429"),
+            LineItem(demandId = "admob", pricefloor = 0.7, adUnitId = "ca-app-pub-9069030271740050/7982361057"),
+            LineItem(demandId = "admob", pricefloor = 1.0, adUnitId = "ca-app-pub-9069030271740050/1416952700"),
+            LineItem(demandId = "admob", pricefloor = 1.5, adUnitId = "ca-app-pub-9069030271740050/6477707698"),
+            LineItem(demandId = "admob", pricefloor = 2.0, adUnitId = "ca-app-pub-9069030271740050/9351283040"),
+            LineItem(demandId = "admob", pricefloor = 3.0, adUnitId = "ca-app-pub-9069030271740050/6286136008"),
+            LineItem(demandId = "admob", pricefloor = 4.0, adUnitId = "ca-app-pub-9069030271740050/3659972663"),
+            LineItem(demandId = "admob", pricefloor = 5.0, adUnitId = "ca-app-pub-9069030271740050/1281221331"),
+            LineItem(demandId = "admob", pricefloor = 7.0, adUnitId = "ca-app-pub-9069030271740050/9842237630")
+        )
+
+        private val applovin = listOf(
+            LineItem(demandId = "applovin", pricefloor = 0.01, adUnitId = "43619c29a94e0269"),
+            LineItem(demandId = "applovin", pricefloor = 0.05, adUnitId = "ce6f29210ba4f0f1"),
+            LineItem(demandId = "applovin", pricefloor = 0.1, adUnitId = "46737e007e374aa8"),
+            LineItem(demandId = "applovin", pricefloor = 0.2, adUnitId = "12ad4228f6acf6d0"),
+            LineItem(demandId = "applovin", pricefloor = 0.3, adUnitId = "8d59ec67c8851e4b"),
+            LineItem(demandId = "applovin", pricefloor = 0.5, adUnitId = "da99849a06674bb6"),
+            LineItem(demandId = "applovin", pricefloor = 0.7, adUnitId = "ae61fbd91eef9699"),
+            LineItem(demandId = "applovin", pricefloor = 1.0, adUnitId = "975f2dcb3e4465fe"),
+            LineItem(demandId = "applovin", pricefloor = 1.5, adUnitId = "c2a6f185d5ff6213"),
+            LineItem(demandId = "applovin", pricefloor = 2.0, adUnitId = "a2ee7eb2294a0790"),
+            LineItem(demandId = "applovin", pricefloor = 3.0, adUnitId = "59f1125269b818f7"),
+            LineItem(demandId = "applovin", pricefloor = 4.0, adUnitId = "f15ca10b889ec5e1"),
+            LineItem(demandId = "applovin", pricefloor = 5.0, adUnitId = "063324ab333adc7e"),
+            LineItem(demandId = "applovin", pricefloor = 7.0, adUnitId = "9ff21515af0cabdc")
+        )
+    }
+}
---
+++ bidon/src/main/java/org/bidon/sdk/ads/interstitial/CachedInterstitialImpl.kt
@@ -0,0 +1,184 @@
+package org.bidon.sdk.ads.interstitial
+
+import android.app.Activity
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
+import org.bidon.sdk.BidonSdk
+import org.bidon.sdk.adapter.AdEvent
+import org.bidon.sdk.adapter.AdSource
+import org.bidon.sdk.adapter.DemandAd
+import org.bidon.sdk.adapter.ext.ad
+import org.bidon.sdk.ads.Ad
+import org.bidon.sdk.ads.AdType
+import org.bidon.sdk.auction.AdTypeParam
+import org.bidon.sdk.config.BidonError
+import org.bidon.sdk.config.impl.asBidonErrorOrUnspecified
+import org.bidon.sdk.databinders.extras.Extras
+import org.bidon.sdk.logs.analytic.AdValue
+import org.bidon.sdk.logs.logging.impl.logInfo
+import org.bidon.sdk.utils.SdkDispatchers
+import org.bidon.sdk.utils.di.get
+import org.bidon.sdk.utils.ext.TAG
+
+internal class CachedInterstitialImpl(
+    dispatcher: CoroutineDispatcher = SdkDispatchers.Main,
+    private val demandAd: DemandAd = DemandAd(AdType.Interstitial)
+) : Interstitial, Extras by demandAd {
+    private val tag get() = TAG
+    private var userListener: InterstitialListener? = null
+    private var observeCallbacksJob: Job? = null
+
+    private val adCache: AdCache by lazy {
+        get {
+            params(demandAd)
+        }
+    }
+    private val listener by lazy {
+        getInterstitialListener()
+    }
+    private val scope by lazy {
+        CoroutineScope(dispatcher)
+    }
+
+    override fun loadAd(activity: Activity, pricefloor: Double) {
+        if (!BidonSdk.isInitialized()) {
+            logInfo(tag, "Sdk is not initialized")
+            listener.onAdLoadFailed(BidonError.SdkNotInitialized)
+            return
+        }
+        if (isReady()) {
+            logInfo(tag, "Ad is loaded and available to show.")
+            return
+        }
+        loadNextAd(activity, pricefloor)
+    }
+
+    override fun showAd(activity: Activity) {
+        if (!BidonSdk.isInitialized()) {
+            logInfo(tag, "Sdk is not initialized")
+            listener.onAdShowFailed(BidonError.SdkNotInitialized)
+            return
+        }
+        logInfo(tag, "Show")
+        val winner = adCache.poll()
+        if (winner != null) {
+            logInfo(tag, "Ad is loaded and available to show.")
+            subscribeToWinner(winner.adSource)
+            scope.launch(Dispatchers.Main.immediate) {
+                (winner.adSource as AdSource.Interstitial).show(activity)
+            }
+        } else {
+            logInfo(tag, "Ad is not loaded. Load it.")
+            listener.onAdShowFailed(BidonError.FullscreenAdNotReady)
+        }
+        val minCachedPricefloor = adCache.peek()?.adSource?.getStats()?.ecpm ?: 0.0
+        loadNextAd(activity, pricefloor = minCachedPricefloor)
+    }
+
+    private fun loadNextAd(activity: Activity, pricefloor: Double) {
+        logInfo(tag, "Load (pricefloor=$pricefloor)")
+        val minCachedPricefloor = adCache.peek()?.adSource?.getStats()?.ecpm ?: 0.0
+        adCache.cache(
+            adTypeParam = AdTypeParam.Interstitial(
+                activity = activity,
+                pricefloor = maxOf(pricefloor, minCachedPricefloor),
+            ),
+            onSuccess = { auctionResult ->
+                logInfo(tag, "Ad loaded ${auctionResult.adSource.ad}")
+                subscribeToWinner(auctionResult.adSource)
+                listener.onAdLoaded(
+                    requireNotNull(auctionResult.adSource.ad) {
+                        "[Ad] should exist when action succeeds"
+                    }
+                )
+            },
+            onFailure = {
+                listener.onAdLoadFailed(cause = it.asBidonErrorOrUnspecified())
+            }
+        )
+    }
+
+    override fun setInterstitialListener(listener: InterstitialListener) {
+        logInfo(tag, "Set interstitial listener")
+        this.userListener = listener
+    }
+
+    override fun notifyLoss(winnerDemandId: String, winnerEcpm: Double) {}
+    override fun notifyWin() {}
+    override fun destroyAd() {}
+
+    override fun isReady(): Boolean {
+        return adCache.peek()?.adSource?.isAdReadyToShow == true
+    }
+
+    /**
+     * Private
+     */
+
+    private fun subscribeToWinner(adSource: AdSource<*>) {
+        require(adSource is AdSource.Interstitial<*>)
+        observeCallbacksJob = adSource.adEvent.onEach { adEvent ->
+            when (adEvent) {
+                is AdEvent.OnReward,
+                is AdEvent.Fill -> {
+                    // do nothing
+                }
+
+                is AdEvent.Clicked -> {
+                    listener.onAdClicked(adEvent.ad)
+                    adSource.sendClickImpression()
+                }
+
+                is AdEvent.Closed -> listener.onAdClosed(adEvent.ad)
+                is AdEvent.Shown -> {
+                    listener.onAdShown(adEvent.ad)
+                    adSource.sendShowImpression()
+                }
+
+                is AdEvent.PaidRevenue -> listener.onRevenuePaid(adEvent.ad, adEvent.adValue)
+                is AdEvent.ShowFailed -> listener.onAdShowFailed(adEvent.cause)
+                is AdEvent.LoadFailed -> listener.onAdLoadFailed(adEvent.cause)
+                is AdEvent.Expired -> listener.onAdExpired(adEvent.ad)
+            }
+        }.launchIn(scope)
+    }
+
+    private fun getInterstitialListener() = object : InterstitialListener {
+        override fun onAdLoaded(ad: Ad) {
+            userListener?.onAdLoaded(ad)
+        }
+
+        override fun onAdLoadFailed(cause: BidonError) {
+            userListener?.onAdLoadFailed(cause)
+        }
+
+        override fun onAdShowFailed(cause: BidonError) {
+            userListener?.onAdShowFailed(cause)
+        }
+
+        override fun onAdShown(ad: Ad) {
+            userListener?.onAdShown(ad)
+        }
+
+        override fun onAdClicked(ad: Ad) {
+            userListener?.onAdClicked(ad)
+        }
+
+        override fun onAdClosed(ad: Ad) {
+            userListener?.onAdClosed(ad)
+        }
+
+        override fun onAdExpired(ad: Ad) {
+            userListener?.onAdExpired(ad)
+        }
+
+        override fun onRevenuePaid(ad: Ad, adValue: AdValue) {
+            userListener?.onRevenuePaid(ad, adValue)
+        }
+    }
+}
