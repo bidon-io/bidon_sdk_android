d3dc77f573916d4e2eaa0e5e582cbb5de62d77c4
AC <ac@bidon.org>
2023-09-25 15:14:35 +0200

    BDN-403. Unity. Prefix Unity BannerPosition

--- app/src/main/java/org/bidon/demoapp/ui/PositionedBannerScreen.kt
+++ app/src/main/java/org/bidon/demoapp/ui/PositionedBannerScreen.kt
@@ -164,7 +164,7 @@ fun PositionedBannerScreen(
                         modifier = Modifier.padding(end = 12.dp),
                         text = "Load",
                     ) {
-                        banner.loadAd(activity, pricefloor = 0.02)
+                        banner.loadAd(activity, pricefloor = 0.001)
                     }
                     AppButton(
                         modifier = Modifier.padding(end = 12.dp),
