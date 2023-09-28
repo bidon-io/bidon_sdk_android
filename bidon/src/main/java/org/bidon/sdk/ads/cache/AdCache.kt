f15bb49e11a24f7e675f75e8c78a9ea8df1a932b
AC <ac@bidon.org>
2023-09-28 14:47:51 +0200

    BDN-431. AdCache. Replaced AuctionHolder with AdCache. (#145)
    * BDN-431. AdCache. Replaced AuctionHolder with AdCache.
    
    * BDN-431. AdCache. Added pricefloor for BigoAds ads

---
+++ bidon/src/main/java/org/bidon/sdk/ads/cache/AdCache.kt
@@ -0,0 +1,38 @@
+package org.bidon.sdk.ads.cache
+
+import org.bidon.sdk.adapter.DemandAd
+import org.bidon.sdk.auction.AdTypeParam
+import org.bidon.sdk.auction.models.AuctionResult
+
+/**
+ * Created by Aleksei Cherniaev on 28/09/2023.
+ */
+internal interface AdCache : Cacheable {
+    val demandAd: DemandAd
+
+    /**
+     * Caches ads.
+     */
+    fun cache(
+        adTypeParam: AdTypeParam,
+        onSuccess: (AuctionResult) -> Unit,
+        onFailure: (Throwable) -> Unit,
+    )
+
+    /**
+     * Exposes only, if exists
+     */
+    fun peek(): AuctionResult?
+
+    /**
+     * Removes from cache if exists and exposes
+     */
+    fun pop(): AuctionResult?
+
+    /**
+     * Waits for the first loaded, then removes from cache and exposes
+     */
+    suspend fun poll(): AuctionResult
+
+    fun clear(onAuctionCancelled: () -> Unit)
+}
---
+++ bidon/src/main/java/org/bidon/sdk/ads/cache/impl/AdCacheImpl.kt
@@ -0,0 +1,152 @@
+package org.bidon.sdk.ads.cache.impl
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.getAndUpdate
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import org.bidon.sdk.adapter.AdEvent
+import org.bidon.sdk.adapter.DemandAd
+import org.bidon.sdk.ads.cache.AdCache
+import org.bidon.sdk.ads.cache.Cacheable
+import org.bidon.sdk.auction.AdTypeParam
+import org.bidon.sdk.auction.Auction
+import org.bidon.sdk.auction.AuctionResolver
+import org.bidon.sdk.auction.models.AuctionResult
+import org.bidon.sdk.logs.logging.impl.logInfo
+import org.bidon.sdk.stats.models.BidStat
+import org.bidon.sdk.utils.di.get
+import org.bidon.sdk.utils.ext.TAG
+
+/**
+ * Created by Aleksei Cherniaev on 28/09/2023.
+ */
+internal class AdCacheImpl(
+    override val demandAd: DemandAd,
+    private val scope: CoroutineScope,
+    private val resolver: AuctionResolver,
+) : AdCache {
+
+    private val tag = "${TAG}_${demandAd.adType.code}"
+    private val isLoading = MutableStateFlow(false)
+    private val results = MutableStateFlow(emptyList<AuctionResult>())
+    private var previousBidStat: BidStat? = null
+    private var previousDemandId: String? = null
+    private var settings: Cacheable.Settings = Cacheable.DefaultSettings
+    private var auction: Auction? = null
+
+    override fun withSettings(settings: Cacheable.Settings) {
+        this.settings = settings
+    }
+
+    override fun cache(
+        adTypeParam: AdTypeParam,
+        onSuccess: (AuctionResult) -> Unit,
+        onFailure: (Throwable) -> Unit,
+    ) {
+        load(adTypeParam, onSuccess, onFailure)
+    }
+
+    override fun peek(): AuctionResult? = results.value.firstOrNull()
+
+    override fun pop(): AuctionResult? {
+        return results.getAndUpdate {
+            it.drop(1)
+        }.firstOrNull()
+    }
+
+    override suspend fun poll(): AuctionResult {
+        val next = results.first { it.isNotEmpty() }.first()
+        previousDemandId = next.adSource.getStats().demandId.demandId
+        previousBidStat = next.adSource.getStats()
+        results.update { it - next }
+        return next
+    }
+
+    override fun clear(onAuctionCancelled: () -> Unit) {
+        results.value = emptyList()
+        auction?.let {
+            it.cancel()
+            onAuctionCancelled()
+        }
+    }
+
+    private fun load(
+        adTypeParam: AdTypeParam,
+        onSuccess: (AuctionResult) -> Unit,
+        onFailure: (Throwable) -> Unit,
+    ) {
+        logInfo(tag, "Cache started: ${results.value.asString()}")
+        if (results.value.size >= settings.minCacheSize) {
+            logInfo(tag, "Cache has enough ads")
+            return
+        }
+        if (!isLoading.getAndUpdate { true }) {
+            logInfo(tag, "Cache ad: $adTypeParam")
+            auction = get()
+            auction?.start(
+                demandAd = demandAd,
+                adTypeParamData = adTypeParam.copy(
+                    pricefloor = maxOf(adTypeParam.pricefloor, results.value.firstOrNull()?.adSource?.getStats()?.ecpm ?: 0.0)
+                ),
+                onSuccess = { auctionResults ->
+                    logInfo(tag, "Auction completed ${results.value.asString()}")
+                    scope.launch {
+                        results.update {
+                            resolver.sortWinners(it + auctionResults).take(settings.cacheCapacity)
+                        }
+                        auctionResults.intersect(results.value.toSet()).forEach { trackExpired(it) }
+                        logInfo(tag, "Auction completed: ${results.value.asString()}")
+                        isLoading.value = false
+                        results.value.firstOrNull()?.let { onSuccess.invoke(it) }
+                    }
+                },
+                onFailure = {
+                    logInfo(tag, "Auction failed: ${results.value.asString()}")
+                    onFailure.invoke(it)
+                    isLoading.value = false
+                },
+            )
+        } else {
+            logInfo(tag, "Ad is already loading")
+        }
+    }
+
+    private fun AdTypeParam.copy(pricefloor: Double): AdTypeParam {
+        return when (val param = this) {
+            is AdTypeParam.Banner -> AdTypeParam.Banner(
+                activity = param.activity,
+                pricefloor = pricefloor,
+                bannerFormat = param.bannerFormat,
+                containerWidth = param.containerWidth
+            )
+
+            is AdTypeParam.Interstitial -> AdTypeParam.Interstitial(
+                activity = param.activity,
+                pricefloor = pricefloor,
+            )
+
+            is AdTypeParam.Rewarded -> AdTypeParam.Rewarded(
+                activity = param.activity,
+                pricefloor = pricefloor,
+            )
+        }
+    }
+
+    private fun trackExpired(actionResult: AuctionResult) {
+        actionResult.adSource.adEvent.onEach { event ->
+            if (event is AdEvent.Expired) {
+                results.update { it - actionResult }
+            }
+        }.launchIn(scope)
+    }
+
+    private fun List<AuctionResult>.asString(): String {
+        return "(${this.size}) " + this.joinToString { auctionResult ->
+            auctionResult.adSource.getStats().let { "${it.demandId.demandId}:${it.ecpm}" }
+        }
+    }
+}
