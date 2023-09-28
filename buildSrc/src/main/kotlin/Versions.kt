82d76425f2b8819879c55f1f679ced7029d12120
AC <ac@bidon.org>
2023-09-28 16:13:43 +0200

    Release 0.4.20

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,7 +1,7 @@
 object Versions {
     private val major = 0
     private val minor = 4
-    private val patch = 18
+    private val patch = 20
     private val semantic: String = ""//""-alpha.2"
 
     val BidonVersionName = mainVersion + semanticVersion
