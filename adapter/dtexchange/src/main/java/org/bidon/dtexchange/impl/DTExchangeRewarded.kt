12ff86928cc8d0839aee6dc210106842fc03b051
AC <ac@bidon.org>
2023-10-31 14:28:10 +0100

    BDN-473. AdUnit. Fixed DTExchange*, Applovin* ad unit obtaining

--- adapter/dtexchange/src/main/java/org/bidon/dtexchange/impl/DTExchangeRewarded.kt
+++ adapter/dtexchange/src/main/java/org/bidon/dtexchange/impl/DTExchangeRewarded.kt
@@ -127,7 +127,15 @@ internal class DTExchangeRewarded :
                         "Error while bidding: $inneractiveErrorCode",
                         inneractiveErrorCode.asBidonError()
                     )
-                    emitEvent(AdEvent.LoadFailed(BidonError.NoFill(demandId)))
+                    emitEvent(
+                        AdEvent.LoadFailed(
+                            if (inneractiveErrorCode == InneractiveErrorCode.ERROR_CONFIGURATION_NO_SUCH_SPOT) {
+                                BidonError.NoAppropriateAdUnitId
+                            } else {
+                                BidonError.NoFill(demandId)
+                            }
+                        )
+                    )
                 }
             }
         )
