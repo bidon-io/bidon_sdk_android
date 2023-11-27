c093cbc7ed174d5a1dde33f70c8861bd366bf838
AC <ac@bidon.org>
2023-11-27 17:28:53 +0100

    Feature/BDN-553. Staging (#165)
    * BDN-553. Add staging to test app
    
    * BDN-553. Staging. Added choosing staging for test app.
    
    * BDN-553. Staging. Added choosing staging for test app.

--- app/src/main/java/org/bidon/demoapp/ui/settings/ServerSettingsScreen.kt
+++ app/src/main/java/org/bidon/demoapp/ui/settings/ServerSettingsScreen.kt
@@ -6,11 +6,14 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Check
@@ -21,31 +24,29 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import org.bidon.demoapp.component.AppButton
 import org.bidon.demoapp.component.AppToolbar
 import org.bidon.demoapp.component.Body1Text
 import org.bidon.demoapp.component.CaptionText
+import org.bidon.demoapp.theme.AppTypography
 import org.bidon.demoapp.theme.getShapeByPositionFor
 import org.bidon.demoapp.ui.settings.data.Host
-
-private const val MockUrl = "https://ef5347ef-7389-4095-8a57-cc78c827f8b2.mock.pstmn.io"
-internal const val AppBaqendBaseUrl = "https://b.appbaqend.com"
 
 @Composable
 internal fun ServerSettingsScreen(
     navController: NavController,
     sharedPreferences: SharedPreferences
 ) {
-    val host = remember {
+    val state = remember {
         mutableStateOf(
-            when (sharedPreferences.getString("host", "")) {
-                AppBaqendBaseUrl -> Host.Production
-                MockUrl -> Host.MockServer
-                else -> Host.Production
-            }
+            Host.values(
+                savedString = sharedPreferences.getString("host", null)
+            )
         )
     }
     Column(
@@ -60,31 +61,33 @@ internal fun ServerSettingsScreen(
                 navController.popBackStack()
             }
         )
-
         RenderHosts(
-            hosts = Host.values().toList(),
-            selectedHost = host.value,
-            onClick = {
-                host.value = it
+            hosts = state.value.hosts,
+            selected = state.value.selected,
+            onClick = { newHost ->
+                state.value = state.value.copy(
+                    selected = newHost,
+                )
+            },
+            onCustomHostChanged = { newHost ->
+                state.value = state.value.copy(
+                    hosts = state.value.hosts.map { currentHost ->
+                        if (currentHost is Host.Staging) {
+                            newHost
+                        } else {
+                            currentHost
+                        }
+                    },
+                    selected = newHost
+                )
             }
         )
-//        RenderPorts(
-//            ports = Ports,
-//            selectedPort = port.value,
-//            onClick = {
-//                port.value = it
-//            }
-//        )
         AppButton(
             modifier = Modifier
                 .padding(16.dp)
                 .align(Alignment.CenterHorizontally),
             onClick = {
-                val hostValue = when (host.value) {
-                    Host.Production -> AppBaqendBaseUrl
-                    Host.MockServer -> MockUrl
-                }
-                sharedPreferences.edit().putString("host", hostValue).apply()
+                sharedPreferences.edit().putString("host", state.value.selected.baseUrl).apply()
                 navController.popBackStack()
             },
             text = "Apply"
@@ -93,7 +96,12 @@ internal fun ServerSettingsScreen(
 }
 
 @Composable
-private fun RenderHosts(hosts: List<Host>, selectedHost: Host, onClick: (Host) -> Unit) {
+private fun RenderHosts(
+    hosts: List<Host>,
+    selected: Host,
+    onClick: (Host) -> Unit,
+    onCustomHostChanged: (Host) -> Unit
+) {
     Column(
         modifier = Modifier
             .padding(horizontal = 16.dp)
@@ -126,15 +134,53 @@ private fun RenderHosts(hosts: List<Host>, selectedHost: Host, onClick: (Host) -
                     Body1Text(
                         text = host.name,
                     )
-                    CaptionText(
-                        text = when (host) {
-                            Host.Production -> AppBaqendBaseUrl
-                            Host.MockServer -> MockUrl
-                        },
-                        maxLines = 1
-                    )
+                    when (host) {
+                        Host.Production -> {
+                            CaptionText(
+                                text = host.baseUrl,
+                                maxLines = 1
+                            )
+                        }
+
+                        Host.MockServer -> {
+                            CaptionText(
+                                text = host.baseUrl,
+                                maxLines = 1
+                            )
+                        }
+
+                        is Host.Staging -> {
+                            Row(
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+                                CaptionText(
+                                    text = Host.Staging.SCHEME,
+                                    maxLines = 1
+                                )
+                                BasicTextField(
+                                    value = host.prefix,
+                                    onValueChange = { newValue ->
+                                        onCustomHostChanged(Host.Staging(newValue))
+                                    },
+                                    textStyle = AppTypography.labelLarge.copy(
+                                        color = MaterialTheme.colorScheme.onPrimary,
+                                        background = MaterialTheme.colorScheme.background.copy(alpha = 0.3f),
+                                    ),
+                                    modifier = Modifier
+                                        .padding(horizontal = 4.dp),
+                                    cursorBrush = SolidColor(MaterialTheme.colorScheme.onBackground),
+                                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                                    maxLines = 1,
+                                )
+                                CaptionText(
+                                    text = Host.Staging.SUFFIX,
+                                    maxLines = 1
+                                )
+                            }
+                        }
+                    }
                 }
-                if (host == selectedHost) {
+                if (selected == host) {
                     Image(
                         painter = rememberVectorPainter(Icons.Outlined.Check),
                         colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.primary),
