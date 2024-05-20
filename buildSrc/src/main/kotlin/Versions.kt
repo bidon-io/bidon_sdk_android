7955c83e31e016a7608ecd883ecc26dcd1e623e4
AZ <az@bidon.org>
2024-05-20 16:01:52 +0200

    Feature/BDN-641: Updated UnityAds SDK to 4.11.3 (#194)

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -11,7 +11,7 @@ object Versions {
         val Applovin = "$mainVersion.2" + semanticVersion
         val BidMachine = "$mainVersion.3" + semanticVersion
         val DTExchange = "$mainVersion.2" + semanticVersion
-        val UnityAds = "$mainVersion.2" + semanticVersion
+        val UnityAds = "$mainVersion.3" + semanticVersion
         val BigoAds = "$mainVersion.2" + semanticVersion
         val Mintegral = "$mainVersion.1" + semanticVersion
         val Vungle = "$mainVersion.1" + semanticVersion
