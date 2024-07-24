251bf93b469a9e430e5f7addc91ce22630550a95
GD <gd@bidon.org>
2024-07-24 11:12:02 +0200

    BDN-705: prepared Bidon 0.6.0;

--- adapter/applovin/build.gradle.kts
+++ adapter/applovin/build.gradle.kts
@@ -21,5 +21,5 @@ dependencies {
     compileOnly(projects.bidon)
     testImplementation(projects.bidon)
 
-    implementation("com.applovin:applovin-sdk:12.4.2")
+    implementation("com.applovin:applovin-sdk:12.6.0")
 }
