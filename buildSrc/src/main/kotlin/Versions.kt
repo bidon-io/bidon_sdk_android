070bde30f2715abb603f63d4a3f2f380748e1bdc
GD <gd@bidon.org>
2024-09-10 15:26:07 +0200

    develop: prepared 0.7.0-next.3;

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -2,7 +2,7 @@ object Versions {
     private val major = 0
     private val minor = 7
     private val patch = 0
-    private val semantic: String = "-next.2"
+    private val semantic: String = "-next.3"
 
     val BidonVersionName = mainVersion + semanticVersion
 
