baa1027096710f905f94074e816aec1648f840bd
GD <gd@bidon.org>
2024-09-26 14:49:54 +0200

    release/0.7-next.5: bump semantic version;

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -2,7 +2,7 @@ object Versions {
     private val major = 0
     private val minor = 7
     private val patch = 0
-    private val semantic: String = "-next.4"
+    private val semantic: String = "-next.5"
 
     val BidonVersionName = mainVersion + semanticVersion
 
