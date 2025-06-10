70300dc65282771948e9b11d89ff3ddaec27b60f
AZ <az@bidon.org>
2025-06-10 13:58:22 +0200

    Feature/BND-961: Set jvm target 11 (#273)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,11 +74,11 @@ android {
         }
     }
     kotlinOptions {
-        jvmTarget = "17"
+        jvmTarget = "11"
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     lint {
         checkReleaseBuilds = false
